77d59244399e15c3c908fa2786415a0b30ce23d3
Justin Bailey <justinb@cs.pdx.edu>
2009-11-07 17:39:09 -0800

    Basic query functionality

--- app/controllers/manage_controller.rb
+++ app/controllers/manage_controller.rb
@@ -1,28 +1,9 @@
 require 'datagrid'
 
 class ManageController < ApplicationController
-  class Group < Struct.new(:id, :name)
-    include Enumerable
-
-    class << self
-      def column_names 
-        ["id", "name"]
-      end
-    end
-
-    def attribute_names
-      ["id", "name"]
-    end
-
-    def each
-      yield ["id", id]
-      yield ["name", name]
-    end
-  end
-
   def index
     @grid = DataGrid.new
-    @display_columns = %w(first_name email is_active)
+    @display_columns = %w(name email company skills house)
     
     @grid.configure do |g|
       g.model = Contact
@@ -31,11 +12,32 @@ class ManageController < ApplicationController
       # g.per_page = 10
 
       g.get_data do |exec, model|
-        model.all
+        if request["query"]["filter"]
+          cond = []
+          if request["query"]["any_skills"]
+            cond << "id in (select contact_id from contact_skills)"
+          elsif request["query"]["skills"]
+            cond << "" 
+          end
+            model.find(:all, :conditions => cond.join(" AND "))
+        else
+            model.all
+        end
       end
 
-      g.get_columns do |exec, model, record|
-        @display_columns.collect { |col| [col, record[col]] }
+      g.get_columns do |exec, model, contact|
+        @display_columns.collect do |col| 
+          case col
+          when "name"
+            [col, "#{contact.first_name} #{contact.last_name}".strip]
+          when "skills"
+            [col, contact.skills.inject { |acc, skill| acc + ", " + skill }.to_s]
+          when "company"
+            [col, contact.company_name]
+          else
+            [col, contact[col].to_s]
+          end
+        end
       end
     end
   end
--- app/models/volunteers.rb
+++ app/models/volunteers.rb
@@ -1,3 +1,20 @@
 class Volunteers < ActiveRecord::Base
   has_many :contacts
+
+  def self.groups
+    latest_project = Project.find_by_sql(<<-SQL).inject(0) { |_, project| project.id }
+select id 
+from projects
+order by created_at DESC
+limit 1
+SQL
+
+    Volunteers.find_by_sql(<<-SQL).collect { |g| g.group_name.to_s }
+select group_name 
+from volunteers
+where project_id = #{latest_project}
+group by group_name
+order by group_name ASC
+SQL
+  end
 end
