3c038523b7d9c306b3216d3b24891633e01d9e1a
jordanlev <github@jordanlev.com>
2009-12-12 10:00:38 -0800

    Adding migration files to prepare for data import

---
+++ db/migrate/20091212094930_modify_skills_schema.rb
@@ -0,0 +1,16 @@
+class ModifySkillsSchema < ActiveRecord::Migration
+  def self.up
+    begin
+      change_column :volunteers, :number_of_people, :integer, :null => false, :default => 1 #Add default=1
+      remove_column :skills, :is_contact_skill
+      remove_column :skills, :is_house_skill
+      add_column :houses, :house_captain_2_contact_id, :integer
+  end
+
+  def self.down
+    remove_column :houses, :house_captain_2_contact_id
+    add_column :skills, :is_contact_skill, :boolean, :null => false, :default => 0
+    add_column :skills, :is_house_skill, :boolean, :null => false, :default => 0
+    change_column :volunteers, :number_of_people, :integer, :null => false
+  end
+end
