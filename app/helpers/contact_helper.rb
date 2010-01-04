cc475f50af9757450a6fe72f33b10c0856db0bb2
jordanlev <github@jordanlev.com>
2010-01-04 11:12:11 -0800

    Moved email field higher up on the contact form. Changed tab order so the 3 phone number fields on the right side of the layout get tabbed to after the contact info. Had to explicitly set tabindex on ALL form fields because the nav links at the top of the page were inserting themselves in the middle of the order otherwise. Created a helper method for incrementing the tab order.

--- app/helpers/contact_helper.rb
+++ app/helpers/contact_helper.rb
@@ -1,2 +1,15 @@
 module ContactHelper
+  
+  @tab_index_counter = 1
+  
+  # "TabIndex" utility function:
+  # Returns a tabindex attribute string with an incremental number.
+  # Starts at tabindex 1 first time called, increments by 1 each subsequent call.
+  # Pass a seed value to (re)start counting at a specific number.
+  def ti(seed = nil)
+    @tab_index_counter = seed || (@tab_index_counter + 1)
+    html = 'tabindex="' + @tab_index_counter.to_s + '"'
+    return html
+  end
+  
 end
