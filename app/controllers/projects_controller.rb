432115d47ab926605daa60c6abb4d879a68a117a
Justin Bailey <justinb@cs.pdx.edu>
2010-01-24 14:13:58 -0800

    Initial projects grid.

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,18 +1,40 @@
 class ProjectsController < ApplicationController
   layout 'mainsite'
+  
+  def initialize
+    @display_columns = %w(project_name starts_on ends_on number_of_volunteers_needed)
     
+  end
+
+  def index
+    @grid = DataGrid.new :grid
+    @grid.configure do |g|
+      g.get_data do |state|
+        Project.find(:all)
+      end
+
+      g.get_columns do |state, house|
+        @display_columns.collect do |col|
+          [col, house[col]]
+        end
+      end
+    end
+  end
+
   def view
     @project = Project.find(params[:id])
   end
+
   def new
     @project = Project.new
   end
+
   def edit
     @project = Project.find(params[:id])
   end
+
   def update
     @project = Project.update(params[:project][:id], params[:project])
     render :view
   end
-  
 end
