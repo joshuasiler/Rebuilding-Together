79fdfd2d64a44d169425d76867d37f3b844d9ad6
Joshua Siler <joshuas@joshuas-linuxvm.(none)>
2010-01-24 14:19:33 -0800

    finished house form

--- app/controllers/houses_controller.rb
+++ app/controllers/houses_controller.rb
@@ -31,18 +31,18 @@ class HousesController < ApplicationController
   
   def view
     @house = House.find(params[:id])
+    @contact = Contact.find(@house.contact_id)
   end
-
   def new
     @house = House.new
   end
-
   def edit
     @house = House.find(params[:id])
+    @contact = Contact.find(@house.contact_id)
   end
-
   def update
     @house = House.update(params[:house][:id], params[:house])
-    render :view
+    @contact = Contact.update(params[:contact][:id], params[:contact])
+    redirect_to "/houses/view/" + params[:house][:id].to_s
   end
 end
