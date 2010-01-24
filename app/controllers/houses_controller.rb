aeb76047d42e5f744408a747b2d7f48e7f873e34
Joshua Siler <joshuas@joshuas-linuxvm.(none)>
2010-01-24 12:38:34 -0800

    base controllers

--- app/controllers/houses_controller.rb
+++ app/controllers/houses_controller.rb
@@ -1,2 +1,11 @@
 class HousesController < ApplicationController
+  def view
+    @house = House.find(params[:id])
+  end
+  def new
+    @house = House.new
+  end
+  def edit
+    @house = House.find(params[:id])
+  end
 end
--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -2,4 +2,10 @@ class ProjectsController < ApplicationController
   def view
     @project = Project.find(params[:id])
   end
+  def new
+    @project = Project.new
+  end
+  def edit
+    @project = Project.find(params[:id])
+  end
 end
