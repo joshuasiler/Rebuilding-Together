7af521c803b39f41523e520c025f9ac76069a7a8
Joshua Siler <joshuas@joshuas-linuxvm.(none)>
2010-05-08 14:36:25 -0700

    plogger updates

--- public/plogger/plog-content/themes/rebuilding/head.php
+++
@@ -1,4 +0,0 @@
-<?php echo plogger_generate_seo_meta_tags();?>
-	<link rel="stylesheet" type="text/css" href="<?php echo THEME_URL ?>gallery.css" />
-	    <link href="/stylesheets/public.css" rel="stylesheet" type="text/css" />
-	<script type="text/javascript" src="<?php echo THEME_URL ?>dynamics.js"></script>
--- public/plogger/plog-content/thumbs/rebuilding-togetherapril-2010-project-day/house-182/large/index.php
+++
@@ -1,3 +0,0 @@
-<?php
-// Ignorance is bliss
-?>
